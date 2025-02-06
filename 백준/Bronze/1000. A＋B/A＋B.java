268ae4649498e4aa26c3105a69f94b576498010c
WonGi-Kim <48573239+WonGi-Kim@users.noreply.github.com>
2025-02-06 20:28:28 +0900

    [Bronze V] Title: A+B, Time: 176 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Main {
+    public void sol(int a, int b) {
+        System.out.println(a+b);
+    }
+    
+    public static void main(String[] args) {
+        Main sol = new Main();
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        sol.sol(a,b);
+    }
+}
